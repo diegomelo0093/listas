13e2fc4dbe4514663bfc59a656694564c055c9aa
diego <diego@diego-PC>
2020-07-21 22:44:44 -0500

    Segundo ejercicio seminario CRUD con listas v2

--- src/colecciones/Empleado.java
+++ src/colecciones/Empleado.java
@@ -18,7 +18,7 @@ public class Empleado {
     private String nombre;
     private String apellido;
     private int salario;
-    
+    public Scanner leer=new Scanner(System.in);
     
     public Empleado(){}
 
@@ -54,19 +54,149 @@ public class Empleado {
        public void setSalario(int salario){
            this.salario=salario;
        }
-       
-      /*
-        public boolean validar(int id){
-            for (Empleado empleado :listaEmpleados)
-            {
-                
-                if(empleado.getId()==id){
-                    return true;
+       public List<Empleado> listaEmpleados = new ArrayList<>();
+       public void cuerpo(){
+        int ids;
+        int menu = 0;
+    while(menu!=5)
+      {
+              System.out.println("Bienvenido al menu estas son las opciones\n"
+            + "1 Para agregar un empleado\n"
+            +"2 Para Eliminar un empleados\n"
+            +"3 Para actualizar un empleado\n"
+            +"4 Para mostrar todos los empleados\n"
+            +"5 Para salir\n");
+            menu=leer.nextInt();
+        switch (menu)
+        {
+            case 1: 
+                menu=1;//agregar datos
+
+                boolean correcto = false;
+                do{
+                System.out.print("\nIngrese el ID: ");
+                ids=leer.nextInt();  
+                for(int i = 0; i < listaEmpleados.size(); i++){
+                  Empleado empleado=listaEmpleados.get(i);
+                  if(empleado.getId()==ids){  
+                      correcto=true;
+                      break;
+                  }else{
+                      correcto=false;
+                  }
                 }
-            }
-            return false;         
+                }
+                while(correcto==true);
+                
+                System.out.print("Ingrese el NOMBRE: ");
+                leer.nextLine();
+                String name=leer.nextLine();
+                System.out.print("Ingrese el APELLIDO: ");
+                String lastname=leer.nextLine();
+                System.out.print("Ingrese el SALARIO:  ");
+                int money=leer.nextInt();
+                Empleado emple=new Empleado(ids ,name,lastname,money);
+                listaEmpleados.add(emple);
+                System.out.println("_______________________________");
+                System.out.println("Empleado creado");
+                System.out.println("Nombre :" + emple.getNombre());
+                System.out.println("apellido :" + emple.getApellido());
+                System.out.println("salario:" + emple.getSalario());
+                System.out.println("ID:" + emple.getId());
+                System.out.println("_______________________________");
+
+                break;
+        
+            case 2: 
+                menu=2;//eliminar datos
+                System.out.println("_______________________________");
+                System.out.println("Estos son los datos");
+                for(int i = 0; i < listaEmpleados.size(); i++){ 
+                Empleado empleado=listaEmpleados.get(i);
+                
+                System.out.println("Nombre:" + empleado.getNombre());
+                System.out.println("apellido:" + empleado.getApellido());
+                System.out.println("salario:" + empleado.getSalario());
+                System.out.println("ID:" + empleado.getId());
+                System.out.println("_______________________________");
+                }
+
+                System.out.println("Ingrese el ID que desea eliminar");
+                int id=leer.nextInt();
+                for(int i = 0; i < listaEmpleados.size(); i++){ 
+                    Empleado empleado=listaEmpleados.get(i);
+                  if(empleado.getId()==id){
+                    listaEmpleados.remove(i);
+                  }      
+                  else{
+                       System.out.print("usuario no creado");
+                  }
+                } 
+                
+            break;
+            case 3: 
+                menu= 3;// actualizar datos
+                System.out.println("_______________________________");
+                System.out.println("Estos son los datos");
+                for(int i = 0; i < listaEmpleados.size(); i++){ 
+                Empleado empleado=listaEmpleados.get(i);
+               
+                System.out.println("Nombre:" + empleado.getNombre());
+                System.out.println("apellido:" + empleado.getApellido());
+                System.out.println("salario:" + empleado.getSalario());
+                System.out.println("ID:" + empleado.getId());
+                System.out.println("_______________________________");
+                }
+                
+                System.out.print("Ingrese el ID que desea actualizar: ");
+                id=leer.nextInt();
+                for(int i = 0; i < listaEmpleados.size(); i++){ 
+                Empleado empleado=listaEmpleados.get(i);
+                  if(empleado.getId()==id){
+                    System.out.print("Ingrese el ID: ");
+                    ids=leer.nextInt();
+                    System.out.print("Ingrese el NOMBRE: ");
+                    leer.nextLine();
+                    name=leer.nextLine();
+                    System.out.print("Ingrese el APELLIDO: ");
+                    lastname=leer.nextLine();
+                    System.out.print("Ingrese el SALARIO: ");
+                    money=leer.nextInt();
+                    System.out.println("EMPLEADO ACTUALIZADO");
+                    empleado.setId(ids);
+                    empleado.setNombre(name);
+                    empleado.setApellido(lastname);
+                    empleado.setSalario(money);
+                  }
+                  else{
+                       System.out.println("usuario no creado");
+                  }
+                }
+               
+                break;
+        
+        
+        
+            case 4: 
+                menu=4;//mostrar los datos
+            
+                 System.out.println("_______________________________");
+                System.out.println("Estos son los datos");
+                for(int i = 0; i < listaEmpleados.size(); i++)
+                { 
+                Empleado empleado=listaEmpleados.get(i);
+                
+                System.out.println("Nombre:" + empleado.getNombre());
+                System.out.println("apellido:" + empleado.getApellido());
+                System.out.println("salario:" + empleado.getSalario());
+                System.out.println("ID:" + empleado.getId());
+                System.out.println("_______________________________");
+                }
+                break;
         }
-        */
+      }
+       }
+
         
         
     @Override
